83a077640374bb25f45f7d066ffbdbfa73dd2afd
Delos Chang <delosfchang@gmail.com>
2013-04-08 20:38:38 -0400

    setting up files

---
+++ views.py
@@ -0,0 +1,49 @@
+import random
+
+def markov_chain():
+    # handle filepaths
+    file_paths = "/Users/deloschang/Documents/self_projects/standalone_markov/corpus.txt" # Bob Dylan songs file input
+    file = open(file_paths)
+
+    # initialize markov dictionary
+    markov_chain = {}
+
+    # 
+    first_word = "\n"
+
+    print "Start reading the lines..."
+
+    for eachline in file:
+        # split by space
+        for current_word in eachline.split():
+
+            # as long as word not empty
+            if current_word != "":
+                markov_chain.setdefault((first_word), []).append(current_word)
+
+                first_word = current_word
+
+    return markov_chain
+ 
+
+def construct_markov(markov_chain, word_count):
+    print "Constructing..."
+
+    generate = ""
+
+    choices = random.choice( markov_chain.keys() )
+    first_word = choices
+    
+    for i in xrange(word_count):
+        new_word = random.choice(markov_chain[(first_word)])
+        generate = generate + " " + new_word
+        first_word = new_word
+        
+    return generate
+
+markov_output = markov_chain()
+
+# default word count to 50 
+print "---Output----"
+print construct_markov(markov_output, 50)
+
